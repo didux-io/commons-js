fa447fdb45ecc1b6c0186b57fb83d731f250f54e
Unknown <mail@mathyn.com>
2018-09-27 16:32:34 +0200

    Added watch mode and option to enable production builds

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 const path = require("path");
 
 module.exports = (env) => {
+    env = env || {};
+    let mode = env.MODE || "development";
+    let watch = env.WATCH == "true";
+
     let module = {
         rules: [
             {
@@ -20,24 +24,28 @@ module.exports = (env) => {
             entry: "./src/index.ts",
             module: module,
             resolve: resolve,
+            mode: mode,
             output: {
                 libraryTarget: "window",
                 library: "Smilo",
                 filename: "smilo-web.js",
                 path: path.resolve(__dirname, "dist")
-            }
+            },
+            watch: watch
         },
         // Node
         {
             entry: "./src/index.ts",
             module: module,
             resolve: resolve,
+            mode: mode,
             output: {
                 libraryTarget: "var",
                 library: "Smilo",
                 filename: "smilo-node.js",
                 path: path.resolve(__dirname, "dist")
-            }
+            },
+            watch: watch
         }
     ]
 }
