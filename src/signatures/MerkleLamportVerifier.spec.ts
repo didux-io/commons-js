04162a617f2e4756bf5448e808328e4cd1c997d9
Unknown <mail@mathyn.com>
2018-10-12 13:56:21 +0200

    Fixed failing unit tests

--- src/signatures/MerkleLamportVerifier.spec.ts
+++ src/signatures/MerkleLamportVerifier.spec.ts
@@ -18,7 +18,7 @@ describe("MerkleLamportVerifier", () => {
             name: "valid1",
             message: "hello world",
             signature: "Zkr6ceBStsK/jY1R:i0p2::i0p3:yFvfFWOHDl5IV8ci::FLkoqREiltsnt0Ys:i0p6::m5lOjM+LbdPcUp+fImJcGP37+D770pepFXFg6noqr2BhVw0M+arvg5Uqr/DK+ZqgXf5jvHHklll/4Zi+vsTZZw==:i0p8,T0+9R9cF7s0JBndiweE4MdNObXhICiZhH1OE80LTcYI=:lQVcLuQ9bnl7txitMfkDtTs9YCuATHk534lS5tVwc0g=:2NbVkchUbqrHxILSiQlofWKk40acTCBD4ZJu9RzdLXk=",
-            address: "X15KPMFXINX6HPCYR7Y2FA2JX7HF54FDMWAJFD",
+            address: "d2+dr9T4+562ySMwKMnMmu3qpjaJ7VK2657SIqNm",
             index: 0,
             layerCount: 4,
             bitCount: 4
@@ -27,7 +27,7 @@ describe("MerkleLamportVerifier", () => {
             name: "valid2",
             message: "hello world",
             signature: "8JXWdDRRGpAyquix:i4p2::i4p3:7bNwxEvpsYenDTIB::e+e1oV/Pi+i8IZD6:i4p6::Zg+mVsWZMFwO5IY4jCKzrgk3XdIf1/JytNzna2AAxSPBPUmcl6alEVOL+x4ouHh3ffKDGysPBAkFlj1GlctEEQ==:i4p8,+5UWCyLdX6QfSMXi8Mi+2dol7vW9p1Nw542lC3O/jOk=:+7SKGMfQhSPYNe6NNPLcYmB2T4hAJZ6RlNWO3lXdKpQ=:2+dr9t4+562ysmWkmNMmU3Qpjaj7Vk2657siQNMcOLM=",
-            address: "X15KPMFXINX6HPCYR7Y2FA2JX7HF54FDMWAJFD",
+            address: "d2+dr9T4+562ySMwKMnMmu3qpjaJ7VK2657SIqNm",
             index: 4,
             layerCount: 4,
             bitCount: 4
@@ -39,7 +39,7 @@ describe("MerkleLamportVerifier", () => {
             // Invalid message
             message: "goodbye world",
             signature: "Zkr6ceBStsK/jY1R:i0p2::i0p3:yFvfFWOHDl5IV8ci::FLkoqREiltsnt0Ys:i0p6::m5lOjM+LbdPcUp+fImJcGP37+D770pepFXFg6noqr2BhVw0M+arvg5Uqr/DK+ZqgXf5jvHHklll/4Zi+vsTZZw==:i0p8,T0+9R9cF7s0JBndiweE4MdNObXhICiZhH1OE80LTcYI=:lQVcLuQ9bnl7txitMfkDtTs9YCuATHk534lS5tVwc0g=:2NbVkchUbqrHxILSiQlofWKk40acTCBD4ZJu9RzdLXk=",
-            address: "X15KPMFXINX6HPCYR7Y2FA2JX7HF54FDMWAJFD",
+            address: "d2+dr9T4+562ySMwKMnMmu3qpjaJ7VK2657SIqNm",
             index: 0,
             layerCount: 4,
             bitCount: 4
@@ -49,7 +49,7 @@ describe("MerkleLamportVerifier", () => {
             message: "hello world",
             // Invalid signature
             signature: "Zkr6ceBStsK/jY1R:i0p1::i0p3:yFvfFWOHDl5IV8ci::FLkoqREiltsnt0Ys:i0p6::m5lOjM+LbdPcUp+fImJcGP37+D770pepFXFg6noqr2BhVw0M+arvg5Uqr/DK+ZqgXf5jvHHklll/4Zi+vsTZZw==:i0p8,T0+9R9cF7s0JBndiweE4MdNObXhICiZhH1OE80LTcYI=:lQVcLuQ9bnl7txitMfkDtTs9YCuATHk534lS5tVwc0g=:2NbVkchUbqrHxILSiQlofWKk40acTCBD4ZJu9RzdLXk=",
-            address: "X15KPMFXINX6HPCYR7Y2FA2JX7HF54FDMWAJFD",
+            address: "d2+dr9T4+562ySMwKMnMmu3qpjaJ7VK2657SIqNm",
             index: 0,
             layerCount: 4,
             bitCount: 4
@@ -59,7 +59,7 @@ describe("MerkleLamportVerifier", () => {
             message: "hello world",
             signature: "Zkr6ceBStsK/jY1R:i0p2::i0p3:yFvfFWOHDl5IV8ci::FLkoqREiltsnt0Ys:i0p6::m5lOjM+LbdPcUp+fImJcGP37+D770pepFXFg6noqr2BhVw0M+arvg5Uqr/DK+ZqgXf5jvHHklll/4Zi+vsTZZw==:i0p8,T0+9R9cF7s0JBndiweE4MdNObXhICiZhH1OE80LTcYI=:lQVcLuQ9bnl7txitMfkDtTs9YCuATHk534lS5tVwc0g=:2NbVkchUbqrHxILSiQlofWKk40acTCBD4ZJu9RzdLXk=",
             // Invalid address
-            address: "X1EPATTSSQASHGLLLMUMOS3OFDCXVBS5GABP5N",
+            address: "dLS84AaOfJ2ypMWFAzo3in/Qmf4GTiEP9BfzfQjh",
             index: 0,
             layerCount: 4,
             bitCount: 4
@@ -68,7 +68,7 @@ describe("MerkleLamportVerifier", () => {
             name: "invalid4",
             message: "hello world",
             signature: "Zkr6ceBStsK/jY1R:i0p2::i0p3:yFvfFWOHDl5IV8ci::FLkoqREiltsnt0Ys:i0p6::m5lOjM+LbdPcUp+fImJcGP37+D770pepFXFg6noqr2BhVw0M+arvg5Uqr/DK+ZqgXf5jvHHklll/4Zi+vsTZZw==:i0p8,T0+9R9cF7s0JBndiweE4MdNObXhICiZhH1OE80LTcYI=:lQVcLuQ9bnl7txitMfkDtTs9YCuATHk534lS5tVwc0g=:2NbVkchUbqrHxILSiQlofWKk40acTCBD4ZJu9RzdLXk=",
-            address: "X15KPMFXINX6HPCYR7Y2FA2JX7HF54FDMWAJFD",
+            address: "d2+dr9T4+562ySMwKMnMmu3qpjaJ7VK2657SIqNm",
             // Invalid index
             index: 3,
             layerCount: 4,
